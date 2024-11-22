b2ab9d8e3db24e06f0ec0660e47ee951d9f08862
Rokas159 <rokas.r.rimkus@gmail.com>
2024-11-22 07:55:27 +0200

    Navigation works

--- web/script.js
+++ web/script.js
@@ -1,7 +1,9 @@
 let nav_toggle = document.getElementById('nav-toggle');
+let nav = document.getElementById('nav-list');
+
+nav.classList.add('hidden');
 
 nav_toggle.addEventListener('click', function() {
-  let nav = document.getElementById('nav-list');
   nav.classList.toggle('hidden');
   console.log("click")
 });
