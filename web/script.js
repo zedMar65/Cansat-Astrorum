25196012953bf46180d92f50de67273665181b7a
Rokas159 <rokas.r.rimkus@gmail.com>
2024-11-22 20:15:59 +0200

    Fixed bugs, made nav menu stick to screen

--- web/script.js
+++ web/script.js
@@ -1,9 +1,8 @@
 let nav_toggle = document.getElementById('nav-toggle');
 let nav = document.getElementById('nav-list');
 
-nav.classList.add('hidden');
-
 nav_toggle.addEventListener('click', function() {
-  nav.classList.toggle('hidden');
+  nav.classList.toggle('extended');
+  nav_toggle.classList.toggle('active');
   console.log("click")
 });
