c0592649a74c591cb2b2f8bcd3515fbc4a64ac17
Rokas-159 <rokas.r.rimkus@gmail.com>
2024-12-11 23:06:27 +0200

    Added a sliding photo gallery

--- web/script.js
+++ web/script.js
@@ -1,8 +1,9 @@
+const bgs = document.querySelectorAll('.bg');
+
 function parallax() {
-    const bgs = document.querySelectorAll('.bg');
     const scrollPosition = window.scrollY;
 
-    bgs.forEach(function(bg) {
+    bgs.forEach(bg => {
         const rect = bg.parentElement.getBoundingClientRect();
         const top = rect.top + scrollPosition;
         const offset = (scrollPosition - top) * 0.5;
@@ -19,3 +20,38 @@ window.addEventListener('scroll', function() {
 });
 
 parallax();
+
+
+
+const galleryNav = document.querySelector('.gallery-nav');
+const gallery = document.querySelector('.gallery');
+const photoParent = document.querySelector('.images');
+
+let navButtons = [];
+
+const photoNames = [
+    "logo.jpg",
+    "logo.png",
+    "parachute.jpg",
+    "placeholder.jpg"
+];
+
+photoNames.forEach(photo => {
+    const button = document.createElement('button');
+    galleryNav.appendChild(button);
+    navButtons.push(button);
+
+    const img = document.createElement('img');
+    img.src = `assets/${photo}`;
+    img.classList.add('gallery-photo');
+    photoParent.appendChild(img);
+
+    button.addEventListener('click', function() {
+        currentPhoto = photoNames.indexOf(photo);
+        photoParent.style.transform = `translateX(-${currentPhoto * 100}%)`;
+        navButtons.forEach(button => button.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+
+navButtons[0].classList.add('active');
