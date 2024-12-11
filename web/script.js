31e3b7a6f7ab34af44b1eec20278dc1b313b4bf1
Rokas159 <rokas.r.rimkus@gmail.com>
2024-12-11 15:15:23 +0200

    Slide show

---
+++ web/script.js
@@ -0,0 +1,21 @@
+function parallax() {
+    const bgs = document.querySelectorAll('.bg');
+    const scrollPosition = window.scrollY;
+
+    bgs.forEach(function(bg) {
+        const rect = bg.getBoundingClientRect();
+        const top = rect.top + scrollPosition;
+        const offset = (scrollPosition - top) * 0.5;
+        bg.style.transform = 'translateY(' + offset + 'px)';
+        
+    });
+
+    // console.log(scrollPosition);
+}
+
+
+window.addEventListener('scroll', function() {
+    parallax();
+});
+
+parallax();
