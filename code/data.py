411d0457173f47da47cdc121c923ae62a02e674f
zedMar65 <marius.jedinkus@gmail.com>
2025-05-17 23:50:51 +0300

    code start

---
+++ code/data.py
@@ -0,0 +1,37 @@
+import ujson
+
+def to_json(obj):
+    return ujson.dumps(obj)
+
+class Data:
+    self.sensors = {}
+    self.data = {}
+    self.radio
+    
+    __init__(self, sensorList, radioChannel):
+        if sensorList.gy_91:
+            self.sensors["sensor_gy"] = SENSOR_GY()
+        if sensorList.radiation:
+            self.sensors["sensor_radiation"] = SENSOR_RADIATION()
+        if sensorList.dust:
+            self.sensors["sensor_dust"] = SENSOR_DUST()
+        if sensorList.gps:
+            self.sensors["sensor_gps"] = SENSOR_GPS()
+        
+        self.radio = RADIO(radioChannel)
+        return
+    
+    def update(self) -> None:
+        keys = self.sensors.keys()
+        for sensor in keys:
+            self.data[sensor] = self.sensors[sensor].update()
+        return
+    
+    def get_data(self) -> str:
+        return self.data
+    
+    def re_init(self) -> None:
+        sensorList = sensors.values()
+        for sensor in sensorList:
+            sensor.__init__()
+        return
---
+++ code/main.py
@@ -0,0 +1,17 @@
+from data import *
+
+extern true
+extern false
+true = True
+false = False
+
+def main() -> None:
+
+def INITATATTATTATA(sensorList) -> None:
+    SensorList SENSORS
+
+    extern data
+    Data data(sensorList)
+
+INITATATTATTATA()
+main()
